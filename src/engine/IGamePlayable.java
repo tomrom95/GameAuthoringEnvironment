d4f41bdff5388bf3a20def76549fcc037c04c264
Ryan St Pierre <ryan.stpierre@duke.edu>
2016-03-30 14:37:01 -0400

    Making slight changes to interfaces

--- src/engine/IGamePlayable.java
+++ src/engine/IGamePlayable.java
@@ -2,10 +2,13 @@ package engine;
 
 import java.util.List;
 
+
 public interface IGamePlayable extends Updateable {
 
     IGameInformation getGameInformation ();
     
     List<? extends Drawable> getDrawables ();
+    
+    List<IAttribute> getGlobalAttributes ();
 
 }
--- src/engine/IGraphicFactory.java
+++ src/engine/IGraphicFactory.java
@@ -1,16 +1,21 @@
 package engine;
 
+import java.util.List;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import util.Block;
 import util.TextGraphic;
 
+
 public interface IGraphicFactory {
 
-	Node getVisual (Block block);
-	
-	Node getVisual (Image image);
-	
-	Node getVisual (TextGraphic text);
-	
+    Node getVisual (Block block);
+
+    Node getVisual (Image image);
+
+    Node getVisual (TextGraphic text);
+
+    VBox renderHUD (List<IAttribute> attributes);
+
 }
