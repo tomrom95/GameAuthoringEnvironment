a32a9709b98be29d54861caa6e8150f78578e21e
Jin An <sa190@duke.edu>
2016-04-03 21:51:51 -0400

    Issue #25 - Added MenuBars

--- src/gameauthoring/AuthoringView.java
+++ src/gameauthoring/AuthoringView.java
@@ -2,9 +2,18 @@ package gameauthoring;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
@@ -33,8 +42,27 @@ public class AuthoringView implements IAuthoringView {
 
     @Override
     public void init (Stage s) {
+        MenuBar menuBar = createMenuBar();
         TabPane tabPane = createAllTabs();
-        s.setScene(new Scene(tabPane, WIDTH, HEIGHT));
+        GridPane root = new GridPane();
+        root.add(menuBar, 0, 0);
+        root.add(tabPane, 0, 2);
+        s.setScene(new Scene(root, WIDTH, HEIGHT));
+    }
+
+    private MenuBar createMenuBar () {
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem saveItem = createMenuItems("Save as XML Files", null); // Connect with Save Actions
+        fileMenu.getItems().add(saveItem);
+        menuBar.getMenus().add(fileMenu);
+        return menuBar;
+    }
+
+    private MenuItem createMenuItems (String itemName, ActionEvent action) {
+        MenuItem newMenuItem = new MenuItem(itemName);
+        newMenuItem.setOnAction(null);
+        return newMenuItem;
     }
 
     private TabPane createAllTabs () {
