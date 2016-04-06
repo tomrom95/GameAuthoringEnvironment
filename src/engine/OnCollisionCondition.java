be906be41b9488cfb40994805053c2adfaedd58c
David Maydew <dmm68@duke.edu>
2016-04-05 20:58:01 -0400

    Implement collision condition

---
+++ src/engine/OnCollisionCondition.java
@@ -0,0 +1,50 @@
+package engine;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import effects.IEffect;
+import javafx.beans.property.ObjectProperty;
+import util.TimeDuration;
+
+
+public class OnCollisionCondition implements ICondition {
+
+    private IGame myGame;
+    private ISpriteGroup myGroupA;
+    private ISpriteGroup myGroupB;
+    private List<IEffect> myApplyToA;
+    private List<IEffect> myApplyToB;
+
+    public OnCollisionCondition (IGame game,
+                                 ISpriteGroup groupA,
+                                 ISpriteGroup groupB,
+                                 List<IEffect> applyToA,
+                                 List<IEffect> applyToB) {
+        myGame = game;
+        myGroupA = groupA;
+        myGroupB = groupB;
+        myApplyToA = applyToA;
+        myApplyToB = applyToB;
+    }
+
+    @Override
+    public void update (TimeDuration duration) {
+        List<ObjectProperty<ISprite>> sprites =
+                myGame.getLevelManager().getCurrentLevel().get().getSprites();
+        for (ISprite outerSprite : sprites.stream()
+                .map(p -> p.get())
+                .filter(sprite -> sprite.getType().equals(myGroupA))
+                .collect(Collectors.toList())) {
+            for (ISprite innerSprite : sprites.stream()
+                    .map(p -> p.get())
+                    .filter(sprite -> sprite.getType().equals(myGroupB))
+                    .collect(Collectors.toList())) {
+                if (outerSprite.getBounds().collide(innerSprite.getBounds())) {
+                    myApplyToA.forEach(effect -> outerSprite.applyEffect(effect));
+                    myApplyToB.forEach(effect -> innerSprite.applyEffect(effect));
+                }
+            }
+        }
+    }
+
+}
