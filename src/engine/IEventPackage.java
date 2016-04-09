8859d44bbffcd9218d69f0f29c8bea562a0701c8
Jon Im <imisjonathan@gmail.com>
2016-04-09 17:50:57 -0400

    Begun refactoring of condtion classes, created event package to hold events and effects, pushing to allow collab

---
+++ src/engine/IEventPackage.java
@@ -0,0 +1,28 @@
+package engine;
+
+import java.util.List;
+import engine.effects.IEffect;
+import engine.events.GameEvent;
+
+
+public interface IEventPackage {
+    
+    /**
+     * The events to be applied
+     * @return
+     */
+    List<GameEvent> getMyEvents ();
+
+    /**
+     * The effects to be applied
+     * @return
+     */
+    List<IEffect> getMyEffects ();
+    
+    /**
+     * The condition will iterate over the members of the game and compare
+     * with this group, selectivity will vary depending on condition
+     * @return
+     */
+    ISpriteGroup getTargetedSpriteGroup();
+}
