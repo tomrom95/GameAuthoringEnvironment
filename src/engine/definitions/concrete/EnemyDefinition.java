59d457357bf2799fb141c7f6bdbf3e7281b5cf6b
Ryan St Pierre <ryan.stpierre@duke.edu>
2016-04-10 20:55:08 -0400

    Almost complete

--- src/engine/definitions/concrete/EnemyDefinition.java
+++ src/engine/definitions/concrete/EnemyDefinition.java
@@ -1,7 +1,27 @@
 package engine.definitions.concrete;
 
+import engine.definitions.SpawnerModuleDefinition;
 import engine.definitions.SpriteDefinition;
+import engine.modules.StaticMover;
+import engine.sprite.ISprite;
 
 public class EnemyDefinition extends SpriteDefinition {
 
+
+    public SpawnerDefinition (SpawnerModuleDefinition spawningModule) {
+        setSpawningModule(spawningModule);
+    }
+    
+    private void setSpawningModule (SpawnerModuleDefinition spawningModule) {
+        mySpawningModule = spawningModule;   
+    }
+
+    public ISprite create () {
+        ISprite sprite = super.create();
+        addModule(mySpawningModule);
+        sprite.initialize(new StaticMover(sprite), createGraphicModule(), createModules(), createAttributes(),
+                          createCoordinate());
+        return sprite;
+
+    }
 }
