68e77b04e691ec8caa9dc8bdd3761f3bd6a54694
Tommy <tomrom95@gmail.com>
2016-04-12 16:37:47 -0400

    small interface change

--- src/facebook_util/actions/HighScoreBoardPost.java
+++ src/facebook_util/actions/HighScoreBoardPost.java
@@ -2,7 +2,7 @@ package facebook_util.actions;
 
 import facebook_util.HighScoreBoard;
 
-public interface HighScoreBoardPost extends SocialAction{
+public interface HighScoreBoardPost extends Post{
 
     public void createBoardPost (HighScoreBoard board);
     
