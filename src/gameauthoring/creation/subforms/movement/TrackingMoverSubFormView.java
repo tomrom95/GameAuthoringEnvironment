404788140ca28d2ac3e1f9ec3df4907248def259
JOE TIMKO <jmt53@duke.edu>
2016-04-12 20:16:47 -0400

    TrackingMoverView complete

---
+++ src/gameauthoring/creation/subforms/movement/TrackingMoverSubFormView.java
@@ -0,0 +1,56 @@
+package gameauthoring.creation.subforms.movement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import gameauthoring.creation.entryviews.IEntryView;
+import gameauthoring.creation.entryviews.TextEntryView;
+import gameauthoring.creation.subforms.SubFormView;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+
+public class TrackingMoverSubFormView extends SubFormView {
+    
+    private GridPane myPane = new GridPane();
+    
+    private String mySpeedKey = "Speed: ";
+    private String myTargetsKey = "Targets: ";
+    
+    private IEntryView mySpeed = new TextEntryView(mySpeedKey, this.getData(), 20, 150, 30);
+    private IEntryView myTargets = new TextEntryView(myTargetsKey, this.getData(), 20, 150, 30);
+    private List<IEntryView> myEntryViews = new ArrayList<IEntryView>(Arrays.asList(mySpeed, myTargets));
+    
+    public TrackingMoverSubFormView(){
+        initView();
+    }
+
+    
+    @Override
+    public Node draw () {
+        // TODO Auto-generated method stub
+        return myPane;
+    }
+
+    @Override
+    public void update () {
+        // TODO Auto-generated method stub
+        
+    }
+
+        
+    private void initView(){
+        super.setMyEntryViews(myEntryViews);
+        myPane.setGridLinesVisible(true);
+        myPane.add(mySpeed.draw(), 0, 0);
+        myPane.add(myTargets.draw(), 0, 1);
+    }
+    
+    public String getSpeedKey(){
+        return mySpeedKey;
+    }
+    
+    public String getTargetsKey(){
+        return myTargetsKey;
+    }
+    
+}
