17f51baa2a1f0caca6dd496bf3d64f452d04acca
Joseph Lilien <jrl48@duke.edu>
2016-04-12 21:34:33 -0400

    modified spriteGroup and deleted GroupDefinition

--- src/engine/SpriteGroup.java
+++ src/engine/SpriteGroup.java
@@ -1,7 +1,12 @@
 package engine;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import engine.definitions.SpriteDefinition;
+import engine.profile.IProfilable;
+import engine.profile.IProfile;
+import engine.profile.Profile;
 import engine.sprite.SpriteType;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,18 +15,35 @@ import javafx.collections.ObservableList;
 /**
  * This class implements ISpriteGroup and holds the notion of a sprite group. A sprite groups is a
  * collection of sprites that are deemed to contain the same attributes and behaviors.
+ * 
+ * @author Joe Lilien, Jeremy Schreck
  *
  */
-public class SpriteGroup implements ISpriteGroup {
+public class SpriteGroup implements ISpriteGroup, IProfilable {
 
-    private ObservableList<SpriteType> mySpriteTypes;
+    private List<SpriteDefinition> mySpriteDefinitions;
+    private List<SpriteType> mySpriteTypes;
+    private IProfile myProfile;
 
-    public SpriteGroup (SpriteType ... spriteTypes) {
-        this(Arrays.asList(spriteTypes));
+    public SpriteGroup () {
+        mySpriteDefinitions = new ArrayList<>();
+        myProfile = new Profile();
     }
 
-    public SpriteGroup (List<SpriteType> spriteTypes) {
-        mySpriteTypes = FXCollections.observableArrayList(spriteTypes);
+    public SpriteGroup (List<SpriteDefinition> spriteTypes) {
+        mySpriteDefinitions = spriteTypes;
+        myProfile = new Profile();
+    }
+
+    public void setSpriteDefinitions (List<SpriteDefinition> sprites) {
+        this.mySpriteDefinitions = new ArrayList<>(sprites);
+        
+        // Patch solution for now, need to work out in more detail later
+        
+        mySpriteTypes = new ArrayList<>();
+        for(SpriteDefinition s: mySpriteDefinitions){
+            mySpriteTypes.add(s.getSpriteType());
+        }
     }
 
     @Override
@@ -31,7 +53,17 @@ public class SpriteGroup implements ISpriteGroup {
 
     @Override
     public ObservableList<SpriteType> getSpriteTypes () {
-        return mySpriteTypes;
+        return FXCollections.observableArrayList(mySpriteTypes);
+    }
+
+    @Override
+    public IProfile getProfile () {
+        return myProfile;
+    }
+
+    @Override
+    public void setProfile (IProfile profile) {
+        this.myProfile = profile;
     }
 
 }
