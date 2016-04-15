79a39addae991871ecf5dc6f9675ba2c2c8f715f
David Maydew <dmm68@duke.edu>
2016-04-15 17:58:55 -0400

    Comment and add todos to sprite package

--- src/engine/sprite/ISprite.java
+++ src/engine/sprite/ISprite.java
@@ -39,7 +39,7 @@ public interface ISprite extends Drawable, Updateable, Affectable, IPositionable
     IMovementModule getMovementStrategy ();
 
     List<IModule> getModules ();
-    
+
     void addModule (IModule module);
 
     List<IResource> getResources ();
@@ -51,7 +51,7 @@ public interface ISprite extends Drawable, Updateable, Affectable, IPositionable
     SpriteType getType ();
 
     boolean shouldBeRemoved ();
-    
-    void setPath(List<Coordinate> path);
+
+    void setPath (List<Coordinate> path);
 
 }
