00717d299ace50833193929932c7f3967028d50b
Jin An <sa190@duke.edu>
2016-04-17 16:58:41 -0400

    Refactored Main UI

--- src/gameauthoring/userinterface/MainUserInterface.java
+++ src/gameauthoring/userinterface/MainUserInterface.java
@@ -1,37 +1,81 @@
 package gameauthoring.userinterface;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
 import gameauthoring.tabs.AuthoringView;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class MainUserInterface {
 
     private static final String TITLE = "Welcome to GitDepends Game Salad";
-    private GridPane myLayout;
+    private static final String RESOURCES_PATH = "resource/MainUserInterface";
+    private static final int SPACING = 20;
+    private ResourceBundle myResources;
+    private Stage myStage;
+    private BorderPane myLayout;
+
+    public MainUserInterface () {
+        myResources = ResourceBundle.getBundle(RESOURCES_PATH);
+    }
+
     public void init (Stage s) {
-        Button createGameBtn = new Button("Create Game");
-        Button loadGameBtn = new Button("Load Existing Game");
-        myLayout = new GridPane();
-        myLayout.add(createGameBtn, 0, 0);
-        myLayout.add(loadGameBtn, 0, 1);
-        createGameBtn.setOnAction(e->createGame(s));
-        loadGameBtn.setOnAction(e->loadGame());
-        Scene scene = new Scene(myLayout,AuthoringView.WIDTH, AuthoringView.HEIGHT);
+        myStage = s;
+        myLayout = new BorderPane();
+        myLayout.setCenter(createButtons());
+        Scene scene = new Scene(myLayout, AuthoringView.WIDTH, AuthoringView.HEIGHT);
         s.setTitle(TITLE);
         s.setScene(scene);
         s.show();
     }
-    
-    private void createGame (Stage s){
-        AuthoringView aView = new AuthoringView();
-        aView.init(s);
+
+    public Node createButtons () {
+        VBox root = new VBox(SPACING);
+        root.setAlignment(Pos.CENTER);
+        Enumeration<String> keys = myResources.getKeys();
+        while (keys.hasMoreElements()) {
+            root.getChildren().add(createButton(keys.nextElement()));
+        }
+        return root;
     }
-    
-    private void loadGame () {
+
+    private Button createButton (String btnName) {
+        Button newButton = new Button(myResources.getString(btnName));
+
+        try {
+            Method method = this.getClass().getMethod(btnName);
+            newButton.setOnAction(event -> callMethod(method));
+        }
+        catch (NoSuchMethodException | SecurityException e) {
+        }
         
+        return newButton;
+
+    }
+
+    private void callMethod (Method method) {
+        try {
+            method.invoke(this);
+        }
+        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            return;
+        }
+    }
+
+    public void createGame () {
+        AuthoringView aView = new AuthoringView();
+        aView.init(myStage);
+    }
+
+    public void loadGame () {
+
     }
 }
