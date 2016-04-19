91f1d35816722e4428308f67d995388617c93e8e
Jin An <sa190@duke.edu>
2016-04-18 23:00:32 -0400

    Added Bitmap data structure for placeable and obstruction, AI

---
+++ src/util/BitMap.java
@@ -0,0 +1,36 @@
+package util;
+
+public class BitMap {
+
+    private boolean[][] myBitMap;
+    private int myWidth;
+    private int myHeight;
+
+    public void createBitMap (int width, int height) {
+        myBitMap = new boolean[width][height];
+        myWidth = width;
+        myHeight = height;
+    }
+
+    public int getHeight () {
+        return myHeight;
+    }
+
+    public int getWidth () {
+        return myWidth;
+    }
+
+    public void flip (int row, int column) {
+        myBitMap[row][column] = !(myBitMap[row][column]);
+    }
+
+    public boolean valueOf (int row, int column) {
+        return myBitMap[row][column];
+    }
+    
+    public void set (int row, int column, boolean value) {
+        myBitMap[row][column] = value;
+    }
+    
+    
+}
