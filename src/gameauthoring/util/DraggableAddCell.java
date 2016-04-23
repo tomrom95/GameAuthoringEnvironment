f7c5f969b0888872f0ea222f43f5ef2a59c4eb76
Joseph Lilien <jrl48@duke.edu>
2016-04-23 15:07:06 -0400

    added new draggable stuff

---
+++ src/gameauthoring/util/DraggableAddCell.java
@@ -0,0 +1,45 @@
+package gameauthoring.util;
+
+import engine.profile.IProfilable;
+import gameauthoring.creation.cellviews.ProfileCellView;
+import gameauthoring.levels.sprites.Draggable;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+
+/**
+ * Draggable cell used to allow user to drag and drop certain items, moving them from one listview to another
+ * 
+ * @author Joe Lilien
+ *
+ * @param <T>
+ */
+public class DraggableAddCell<T extends IProfilable> extends DraggableCell<T>{    
+    
+    ListView<T> myTarget;
+    
+    public DraggableAddCell(ListView<T> target){
+        setTarget(target);
+        myTarget = target;
+    }
+    
+    @Override
+    public void setOnDragOver (DragEvent e) {
+        e.acceptTransferModes(TransferMode.COPY);
+    }
+
+    @Override
+    public void setOnDragDropped (DragEvent e) {
+        Dragboard db = e.getDragboard();
+        if (db.hasString()) {
+            if(!myTarget.getItems().contains(getProfilable())){
+                myTarget.getItems().add(getProfilable());
+            }
+        }
+    }
+    
+
+}
---
+++ src/gameauthoring/util/DraggableRemoveCell.java
@@ -0,0 +1,38 @@
+package gameauthoring.util;
+
+import engine.profile.IProfilable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+
+
+public class DraggableRemoveCell<T extends IProfilable> extends DraggableCell<T> {
+
+    private static final double PIC_SIZE = 150;
+    private UIFactory myUIFactory = new UIFactory();
+
+    public DraggableRemoveCell (Node target) {
+        setTarget(target);
+    }
+
+    @Override
+    protected Node createSpriteCell (T profile) {
+        Label label = new Label(profile.getProfile().getName().get());
+        Node image = createImageProfile(profile.getProfile(), PIC_SIZE);
+        Node node =  myUIFactory.makeVBox(10, image , label);
+        setActions(node);
+        return node;
+    }
+
+    @Override
+    public void setOnDragOver (DragEvent e) {
+        e.acceptTransferModes(TransferMode.ANY);
+    }
+
+    @Override
+    public void setOnDragDropped (DragEvent e) {
+        getListView().getItems().remove(getIndex());
+    }
+
+}
