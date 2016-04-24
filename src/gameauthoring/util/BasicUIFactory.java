d0efada713807bc2b760b7752b9c94e7e36c9b66
Joseph Lilien <jrl48@duke.edu>
2016-04-24 15:07:23 -0400

    trying to fix conflicts

--- src/gameauthoring/util/BasicUIFactory.java
+++ src/gameauthoring/util/BasicUIFactory.java
@@ -7,6 +7,7 @@ import engine.rendering.ScaleFactory;
 import gameauthoring.creation.cellviews.NameCellView;
 import graphics.IGraphic;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -80,11 +81,6 @@ public class BasicUIFactory implements UIFactory {
         return newButton;
     }
     
-    public Button createImageButton (String url) {
-        Image image = new Image(url);
-        Button newButton = new Button(null, new ImageView(image));
-        return newButton;
-    }
 
     public Tab createTab (String text, boolean closable, Node content) {
         Tab newTab = new Tab();
@@ -94,55 +90,7 @@ public class BasicUIFactory implements UIFactory {
         return newTab;
     }
 
-    public Image getImageFromNode (Node node) {
-        return node.snapshot(new SnapshotParameters(), null);
-    }
 
-    public <T extends IProfilable> ComboBox<T> createCombo (ObservableList<T> list) {
-        ComboBox<T> box = new ComboBox<>(list);
-        addCellFactory(box);
-        return box;
-    }
-
-    public TextField createTextField (double width) {
-        TextField text = new TextField();
-        text.setMaxWidth(width);
-        return text;
-    }
-
-    private <T extends IProfilable> void addCellFactory (ComboBox<T> comboBox) {
-        comboBox.setCellFactory(c -> new NameCellView<>());
-        comboBox.setButtonCell(new NameCellView<>());
-
-    }
-
-    public Optional<String> getTextDialog (String holder,
-                                           String title,
-                                           String content) {
-        TextInputDialog dialog = new TextInputDialog(holder);
-        dialog.setTitle(title);
-        dialog.setContentText(content);
-        return dialog.showAndWait();
-
-    }
-
-    public Label createLabel (String title) {
-        return new Label(title);
-    }
-    
-    public Label createTitleLabel (String title) {
-        Label label = createLabel(title);
-        addStyling(label, "TitleLabel");
-        return label;
-    }
-
-
-    public Node createTitledSlider (String title, Slider s) {
-        VBox vbox = new VBox (CUSHION);
-        vbox.getChildren().add(createLabel(title));
-        vbox.getChildren().add(s);
-        return vbox;
-    }
     
     public Button createImageButton (Node imgview,
                                      EventHandler<ActionEvent> action) {
@@ -328,33 +276,5 @@ public class BasicUIFactory implements UIFactory {
     private void addStyling(Node node, String key) {
         node.getStyleClass().add(myStyle.getString(key));
     }
-    
-    public Slider createSlider (double min, double max, double start, double increment) {
-        Slider slider = new Slider();
-        slider.setMin(min);
-        slider.setMax(max);
-        slider.setValue(start);
-        slider.setBlockIncrement(increment);
-        slider.setShowTickLabels(true);
-        slider.setShowTickMarks(true);
-        return slider;
-    }
 
-    public Node createImageView (IGraphic image, double width, double height) {
-        return image.getVisualRepresentation(new ScaleFactory(width, height));
-    }
-
-    public Button createButton (String title) {
-        return new Button(title);
-    }
-
-    public Label createSubTitleLabel (String string) {
-        Label label = createLabel(string);
-        addStyling(label, "SubTitleLabel");
-        return label;
-    }
-    
-    protected void addStyling(Node node, String key) {
-        node.getStyleClass().add(myStyle.getString(key));
-    }
 }
