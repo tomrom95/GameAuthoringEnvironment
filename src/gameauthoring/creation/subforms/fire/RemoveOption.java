56cd88b260bceca8cfe6b50071c9f683a54c7ee2
Joseph Lilien <jrl48@duke.edu>
2016-04-24 13:48:54 -0400

    added remove class and more images

---
+++ src/gameauthoring/creation/subforms/fire/RemoveOption.java
@@ -0,0 +1,40 @@
+package gameauthoring.creation.subforms.fire;
+
+import gameauthoring.creation.subforms.SubFormView;
+import gameauthoring.util.Glyph;
+import gameauthoring.util.UIFactory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+
+
+/**
+ * Easy implementation and reusable class for adding a context menu to an item with some sort of
+ * remove functionality, defined in constructor, helps keep separation of model and view
+ * 
+ * @author Joe Lilien
+ *
+ */
+public class RemoveOption implements Glyph{
+
+    private Button removeMenu;
+    private UIFactory myUIFactory = new UIFactory();
+    
+    public RemoveOption(EventHandler<ActionEvent> e){
+        initRemove(e);
+    }
+
+    private void initRemove (EventHandler<ActionEvent> e) {
+        removeMenu = myUIFactory.createImageButton(myUIFactory.makeImageView(("images/close.png"), 20, 20), e);
+    }
+    
+   
+    @Override
+    public Node draw () {
+        return  removeMenu;
+    }
+
+}
