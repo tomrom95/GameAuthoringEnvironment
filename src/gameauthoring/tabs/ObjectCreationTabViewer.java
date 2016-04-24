ee3d48db2c594740189718a1110edf5469a9c0e2
David Maydew <dmm68@duke.edu>
2016-04-24 14:42:16 -0400

    Fix authoring property error

--- src/gameauthoring/tabs/ObjectCreationTabViewer.java
+++ src/gameauthoring/tabs/ObjectCreationTabViewer.java
@@ -91,7 +91,7 @@ public class ObjectCreationTabViewer implements ITabViewer {
                                                    myGame);
         CreationController<?> ccDefenders =
                 ccFactory.createCreationController(myControllerResources.getString("Defenders"),
-                                                   myLang.getString("Defender"), myDefenderSFCs,
+                                                   myLang.getString("Defenders"), myDefenderSFCs,
                                                    myGame);
         CreationController<?> ccGroups =
                 ccFactory.createCreationController(myControllerResources.getString("Groups"),
