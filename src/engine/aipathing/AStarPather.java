ab032f9efbb98b8ca6bf436e933a7ae8724fbaf8
Jon Im <imisjonathan@gmail.com>
2016-04-24 12:49:23 -0400

    Added IHeuristic interface and straight line heuristic

--- src/engine/aipathing/AStarPather.java
+++ src/engine/aipathing/AStarPather.java
@@ -1,16 +1,50 @@
 package engine.aipathing;
 
+import java.util.ArrayList;
 import java.util.List;
 import util.Coordinate;
 import util.IBitMap;
 
-
+/**
+ * Implementation of A* search based off of the pseudo-code provided
+ * at <a href="web.mit.edu/eranki/www/tutorials/search">Pathfinding using A* (A-Star)</a>
+ * 
+ * @author jonathanim
+ *
+ */
 public class AStarPather implements INodeGraphPather {
+    private IHeuristic myHeuristic;
 
+    public AStarPather () {
+        myHeuristic = new StraightLineHeuristic();
+    }
+    
     @Override
-    public List<Coordinate> findPathFor (IBitMap obstructionMap, Coordinate start, Coordinate goal) {
-        // TODO Auto-generated method stub
+    public List<Coordinate> findPathFor (IBitMap obstructionMap,
+                                         Coordinate start,
+                                         Coordinate goal) {
+        INodeGraphFactory graphFactory = new GameGraphFactory(obstructionMap);
+        INodeGraph graph = graphFactory.getConstructedGraph();
+        IPathNode startNode = graph.getClosestNode(start);
+        IPathNode goalNode = graph.getClosestNode(goal);
+        if (startNode == null || goalNode == null) {
+            return new ArrayList<>();
+        }
+
+        return aStar(startNode, goalNode, graph, getHeuristic());
+    }
+    
+    private List<Coordinate> aStar (IPathNode startNode,
+                                    IPathNode goalNode,
+                                    INodeGraph graph,
+                                    IHeuristic heuristic) {
+
         return null;
+    }
+    
+    public IHeuristic getHeuristic () {
+        return myHeuristic;
     }
 
 }
+
