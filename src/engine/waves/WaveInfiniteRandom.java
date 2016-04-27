9e5eadc39e3f67c2cd38f73e3b02ca13897ef100
JOE TIMKO <jmt53@duke.edu>
2016-04-26 22:29:21 -0400

    no more print statements

---
+++ src/engine/waves/WaveInfiniteRandom.java
@@ -0,0 +1,12 @@
+package engine.waves;
+
+import java.util.List;
+
+public class WaveInfiniteRandom extends Wave {
+
+    public WaveInfiniteRandom (List<WaveBlock> sprites) {
+        super(sprites);
+        // TODO Auto-generated constructor stub
+    }
+
+}
