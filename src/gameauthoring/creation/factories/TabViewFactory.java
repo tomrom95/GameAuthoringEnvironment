0866109f13abbb8de3c74ecf81d4ea510cf25f19
Dhrumil Patel <dhrumil.r.patel95@gmail.com>
2016-04-29 17:26:55 -0400

    created tab view factory

---
+++ src/gameauthoring/creation/factories/TabViewFactory.java
@@ -0,0 +1,14 @@
+package gameauthoring.creation.factories;
+
+import gameauthoring.tabs.ITabViewer;
+
+/**
+ * Creating a factory to generate the views required for the 
+ * authoring environment
+ * 
+ * @author Dhrumil
+ *
+ */
+public class TabViewFactory<T extends ITabViewer> {
+
+}
