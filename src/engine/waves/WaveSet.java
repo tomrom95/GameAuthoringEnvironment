b75e8b8566ec3895d39f9a93419cf3a3903d5624
JOE TIMKO <jmt53@duke.edu>
2016-04-29 11:54:40 -0400

    actually got rid of system out

--- src/engine/waves/WaveSet.java
+++ src/engine/waves/WaveSet.java
@@ -37,7 +37,6 @@ public class WaveSet implements IWaveSet {
 
     @Override
     public boolean allWavesCompleted () {
-        System.out.println(myWaves + " current waves: "+currentWave);
         return (myWaves.size() == 0) & currentWave.waveCompleted();
 
     }
