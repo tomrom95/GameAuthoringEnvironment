6e680a2d30e8aca2c232f9925721b0b247845868
Dhrumil Patel <dhrumil.r.patel95@gmail.com>
2016-04-29 19:08:01 -0400

    refactored authoring tabs using resources and factory design pattern

--- src/gameauthoring/creation/factories/TabViewFactory.java
+++ src/gameauthoring/creation/factories/TabViewFactory.java
@@ -1,10 +1,17 @@
 package gameauthoring.creation.factories;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 import engine.IGame;
 import gameauthoring.tabs.ITabViewer;
+import gameauthoring.util.BasicUIFactory;
+import javafx.scene.control.Tab;
+import util.BundleOperations;
+
 
 /**
- * Creating a factory to generate the views required for the 
+ * Creating a factory to generate the tab views required for the
  * authoring environment
  * 
  * @author Dhrumil
@@ -12,11 +19,48 @@ import gameauthoring.tabs.ITabViewer;
  */
 public class TabViewFactory<T extends ITabViewer> {
 
+    private static final String IMAGES = "defaults/create_tab_images";
+    private static final String TAB_NAMES = "defaults/create_tabs";
+    private static final String TAB_VIEWER_NAMES = "defaults/create_tabviewers";
+
     private IGame myGame;
+    private ResourceBundle myImages;
+    private ResourceBundle myTabs;
+    private ResourceBundle myTabViewers;
+    private BasicUIFactory myUIFactory;
 
     public TabViewFactory (IGame game) {
         myGame = game;
+        myImages = ResourceBundle.getBundle(IMAGES);
+        myTabs = ResourceBundle.getBundle(TAB_NAMES);
+        myTabViewers = ResourceBundle.getBundle(TAB_VIEWER_NAMES);
+        myUIFactory = new BasicUIFactory();
     }
-    
-    
+
+    private ITabViewer createTabViewer (String tabViewerName) {
+        return (ITabViewer) Reflection.createInstance(tabViewerName, myGame);
+
+    }
+
+    /**
+     * Generates tabs with its associated view and resources
+     * 
+     * @return list of available tabs to render
+     */
+    public List<Tab> createTabs () {
+        List<Tab> tabList = new ArrayList<>();
+        List<String> tabViewerNames =
+                BundleOperations.getPropertyValueAsList("Order", myTabViewers);
+
+        for (String tabName : tabViewerNames) {
+            tabList.add(myUIFactory
+                    .createTabGraphic(myUIFactory.makeImageDisplay(myImages.getString(tabName),
+                                                                   myTabs.getString(tabName)),
+                                      false, createTabViewer(tabName)
+                                              .draw()));
+        }
+
+        return tabList;
+    }
+
 }
