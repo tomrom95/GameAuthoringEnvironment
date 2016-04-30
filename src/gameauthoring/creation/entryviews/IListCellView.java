65ec8356a2c4b30b3ab3a9119397458ac9254947
JOE TIMKO <jmt53@duke.edu>
2016-04-30 13:48:57 -0400

    ImageEntryView checkstyle, STILL TODOS HERE

--- src/gameauthoring/creation/entryviews/IListCellView.java
+++ src/gameauthoring/creation/entryviews/IListCellView.java
@@ -3,20 +3,22 @@ package gameauthoring.creation.entryviews;
 import java.util.function.Consumer;
 import gameauthoring.util.Glyph;
 
+
 /**
  * Interface for defining how a "List Item" looks
  * during creation. For example, when making sprites,
  * this will show a sprite image, name, and description
  * on the side.
+ * 
  * @author Tommy, Jeremy Schreck
  *
  */
-public interface IListCellView<E> extends Glyph{
+public interface IListCellView<E> extends Glyph {
 
     /**
      * Tell the view which method it should call to edit a previously created item
      * 
      * @param action The action to take when the user decides to edit a different item
      */
-    void setEditAction(Consumer<E> action);
+    void setEditAction (Consumer<E> action);
 }
